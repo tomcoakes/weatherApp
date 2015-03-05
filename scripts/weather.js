26def7bfadb2c48a02df59414a4fd785b6e7426d
Tom Coakes <tomcoakes@gmail.com>
2015-03-05 17:21:49 +0000

    Refactored weather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,37 +1,45 @@
 $( "form" ).submit(function() {
   ( result = $("input:first" ).val());
-  
-  $.getJSON('http://api.openweathermap.org/data/2.5/weather?q='+result, function(data) {
-
-      status = data.cod;
-
-      $('.icon').attr('class','icon');
-
-      if(status == 200) {
-
-        temp = Math.round(data.main.temp - 273.15);
-        city = data.name;
-        conditions = data.weather[0].main;
-        conditionsCode = data.weather[0].id;
-        country = data.sys.country;
-        weatherIcon = weather(conditionsCode);
-
-        $('.icon').addClass(weatherIcon);
-
-        $('.temperature').text(temp);
-        $('.city').text(city+", "+country);
-        $('img').attr('src', 'images/' + weatherIcon + '.svg');
-
-      } else {
-        $('.city').text("City not recognised");
-      };
-  });
+  requestApi(); 
 });
 
-weather = function(code) {
-  if(code < 600) { result = "rain"};
-  if(code < 700) { result = "snow"};
-  (code < 802 || code == 904) ? result = "sunny" : result = "cloudy";
+requestApi = function() {
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather?q='+result, function(data) {
+    resetBackgroundColor();
+    (data.cod != 200) ? ($('.city').text("City not recognised")) : retrieveApiData(data);
+    weatherIcon = weatherConditions(conditionsCode);
+    displayData();
+  });
+};
+
+retrieveApiData = function(data) {
+  temp = convertToCelsius(data.main.temp);
+  city = data.name;
+  conditions = data.weather[0].main;
+  conditionsCode = data.weather[0].id;
+  country = data.sys.country;
+};
+
+displayData = function() {
+  $('.icon').addClass(weatherIcon);
+  $('.temperature').text(temp);
+  $('.city').text(city+", "+country);
+  $('img').attr('src', 'images/' + weatherIcon + '.svg');  
+};
+
+convertToCelsius = function(kelvin) {
+  return (Math.round(kelvin - 273.15));
+};
+
+resetBackgroundColor = function() {
+  $('.icon').attr('class','icon');
+};
+
+weatherConditions = function(code) {
+  if(code >= 802 && code < 805) {result = "cloudy"};
+  if(code < 802 || code == 904) {result = "sunny"};
+  if(code < 700) {result = "snow"};
+  if(code < 600) {result = "rain"};
   return result;
 };
 
