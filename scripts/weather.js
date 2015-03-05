bc56b06ce9d6b1dc54e6236dc5e35d193b2a86b8
Yannick Brunner <yannickbrunner@gmail.com>
2015-03-05 14:30:42 +0000

    still having issue with color, tied to refresh

--- scripts/weather.js
+++ scripts/weather.js
@@ -4,25 +4,30 @@ $( "form" ).submit(function() {
   ( result = $("input:first" ).val());
   
   $.getJSON('http://api.openweathermap.org/data/2.5/weather?q='+result, function(data) {
-    status = data.cod;
     
-    if(status == 200) {
-      temp = Math.round(data.main.temp - 273.15);
-      city = data.name;
-      conditions = data.weather[0].main;
-      conditionsCode = data.weather[0].id;
-      country = data.sys.country;
-      weatherIcon = weather(conditionsCode);
-      $("div.weather").toggleClass(function() {
-        return (weather(conditionsCode));
-      });
-      $('.temperature').text(temp);
-      $('.city').text(city+", "+country);
-      $('img').attr('src', 'images/' + weatherIcon + '.svg');
+    setTimeout(function() {
 
-    } else {
-      $('.city').text("City not recognised");
-    };
+      status = data.cod;
+
+      if(status == 200) {
+        temp = Math.round(data.main.temp - 273.15);
+        city = data.name;
+        conditions = data.weather[0].main;
+        conditionsCode = data.weather[0].id;
+        country = data.sys.country;
+        weatherIcon = weather(conditionsCode);
+        $("div.weather").toggleClass(function() {
+          return (weather(conditionsCode));
+        });
+        $('.temperature').text(temp);
+        $('.city').text(city+", "+country);
+        $('img').attr('src', 'images/' + weatherIcon + '.svg');
+
+      } else {
+        $('.city').text("City not recognised");
+      };
+
+    }, 0);
   });
 });
 
