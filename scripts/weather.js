1025bdaf0892742f4613bb0311742f546af52a7d
Tom Coakes <tomcoakes@gmail.com>
2015-03-05 11:25:23 +0000

    Using openweathermap api to pull in london temperature

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,3 +1,9 @@
+
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=london', function(data) {
+  temp = data["main"]["temp"];
+});
+
+
 var apiData = ["London", "Paris"];
 var temperature = 22;
 var weatherIcon = 'snow';
